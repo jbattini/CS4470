6dfab8afe1fcf750261c5e0294d4248f33961f5e
KasperSeas <kevinravimowers@gmail.com>
2016-10-05 13:49:49 -0700

    everything working, bug fixes

--- src/main/Client.java
+++ src/main/Client.java
@@ -23,7 +23,7 @@ public class Client extends Thread {
 	         System.out.println("Connecting to " + destination + " on port " + port);
 	         socket = new Socket(destination, port, this.localAddress, localPort);
 	         
-	         System.out.println("Just connected to " + socket.getRemoteSocketAddress());
+	         System.out.println("Socket created " + socket.getRemoteSocketAddress());
 	         
 	         OutputStream outToServer = socket.getOutputStream();
 	         out = new DataOutputStream(outToServer);
@@ -35,10 +35,22 @@ public class Client extends Thread {
 	         
 	         System.out.println("Server says " + in.readUTF());
 	         start();
-	      } catch(IOException e) {
-	         e.printStackTrace();
+	      } catch(UnknownHostException uhe) {
+	    	 System.out.println("Host address doesn't exist!");
+		     //uhe.printStackTrace();
+		  } catch(BindException be) {
+			 System.out.println("Socket is already in use!");
+		     //be.printStackTrace();
+		  } catch(ConnectException ce) {
+			  System.out.println("Host port doesn't exist or duplicate connection");
+		      //ce.printStackTrace();
+		  } catch(SocketException se) {
+			  System.out.println("Cannot connect to self-connection!");
+		         //e.printStackTrace();
+		  } catch(IOException e) {
+			  System.out.println("IO Exception has occured!");
+	         //e.printStackTrace();
 	      } 
-		
 		
 		
 	}
@@ -53,7 +65,12 @@ public class Client extends Thread {
 	         }
 	         catch(IOException ioe)
 	         {  System.out.println("Listening error: " + ioe.getMessage());
-	            close();
+	            try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	            break;
 	         }
 	      }
 	}
