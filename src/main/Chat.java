827dae7c4a50765b26e80364dc330b724b05d8b3
Juan Battini <juanbattini@live.com>
2016-10-06 13:26:39 -0700

    ooops
    added terminate method to server class and call to speaktoserver in chat class

--- src/main/Chat.java
+++ src/main/Chat.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 import java.net.*;
 
 public class Chat {
-	
-	//static Thread server;
+
+	// static Thread server;
 	static Client client;
 	static public int localPort;
 	private static Scanner in;
-	
-	public static void main(String [] args) {
-	      localPort = Integer.parseInt(args[0]);	      
-	      in = new Scanner(System.in);
-	      String input;
-	      do {
+
+	public static void main(String[] args) {
+		localPort = Integer.parseInt(args[0]);
+		in = new Scanner(System.in);
+		String input;
+		do {
 			input = in.nextLine();
 			command(input);
-	      } while(!input.equals("8"));
-	 }
-	
-	public static void command(String command){
+		} while (!input.equals("8"));
+	}
+
+	public static void command(String command) {
 		String[] args = command.split(" ");
 		String cmd1 = args[0];
 		String cmd2 = "";
@@ -30,95 +30,100 @@ public class Chat {
 			cmd2 = args[1];
 		}
 		if (args.length > 2) {
-			
-			for(int i = 2; i < args.length; i++){
+
+			for (int i = 2; i < args.length; i++) {
 				cmd3 += args[i] + " ";
 			}
 		}
-		
-		
-		
+
 		switch (cmd1) {
-        case "help":  help();
-                 break;
-                 
-        case "myip":  try {
-        		myip();
+		case "help":
+			help();
+			break;
+
+		case "myip":
+			try {
+				myip();
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			}
-                 break;
-        case "myport":  try {
+			break;
+		case "myport":
+			try {
 				myport();
 			} catch (UnknownHostException e1) {
 				e1.printStackTrace();
 			}
-                 break;
-        case "connect": 
-        	String destination = cmd2;
-        	int port = Integer.parseInt(args[2]);
-        	try {
+			break;
+		case "connect":
+			String destination = cmd2;
+			int port = Integer.parseInt(args[2]);
+			try {
 				client = new Client(destination, port, localPort);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-            break;
-        case "list": client.speakToServer(args);
-                 break;
-        case "terminate": 
-                 break;
-        case "send": 
-        	if (cmd3.length() > 100) {
+			break;
+		case "list":
+			client.speakToServer(args);
+			break;
+		case "terminate": client.speakToServer(args);
+			break;
+		case "send":
+			if (cmd3.length() > 100) {
 				System.out.println("Your message is more than 100 characters!");
 			} else {
-//				String[] sendMsgArgs = new String[3];
-//				sendMsgArgs[0] = cmd1;
-//				sendMsgArgs[1] = cmd2;
-//				sendMsgArgs[2] = cmd3;
+				// String[] sendMsgArgs = new String[3];
+				// sendMsgArgs[0] = cmd1;
+				// sendMsgArgs[1] = cmd2;
+				// sendMsgArgs[2] = cmd3;
 				client.speakToServer(args);
 			}
-                 break;
-        case "exit":  exit();
-                 break;
+			break;
+		case "exit":
+			exit();
+			break;
 		}
-		
+
 	}
-	
-	
-	
-	public static void help(){
+
+	public static void help() {
 		System.out.println();
 		System.out.println("====================HELP======================");
 		System.out.println("help:  Display information about the available user interface options or command manual.");
 		System.out.println("myip:  Display the IP address of this process.");
 		System.out.println("myport:  Display the port on which this process is listening for incoming connections.");
-		System.out.println("connect <destination> <port no>:  This command establishes a new TCP connection to the specified <destination> at the specified < port no>. The <destination> is the IP address of the computer.");
+		System.out.println(
+				"connect <destination> <port no>:  This command establishes a new TCP connection to the specified <destination> at the specified < port no>. The <destination> is the IP address of the computer.");
 		System.out.println("list:  Display a numbered list of all the connections this process is part of.");
-		System.out.println("terminate <connection id>:  This command will terminate the connection listed under the specified number when LIST is used to display all connections.");
-		System.out.println("send <connection id> <message>:  This will send the message to the host on the connection that is designated by the number 3 when command ‚Äúlist‚Äù is used. ");
-		System.out.println("exit:  Close all connections and terminate this process. The other peers should also update their connection list by removing the peer that exits.");
+		System.out.println(
+				"terminate <connection id>:  This command will terminate the connection listed under the specified number when LIST is used to display all connections.");
+		System.out.println(
+				"send <connection id> <message>:  This will send the message to the host on the connection that is designated by the number 3 when command ìlistî is used. ");
+		System.out.println(
+				"exit:  Close all connections and terminate this process. The other peers should also update their connection list by removing the peer that exits.");
 		System.out.println("==============================================");
 		System.out.println();
 	}
-	
-	public static void myip() throws UnknownHostException{
+
+	public static void myip() throws UnknownHostException {
 		InetAddress address = null;
 		try {
 			address = InetAddress.getLocalHost();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 		
+		}
 		System.out.println("My IP Address: " + address);
 	}
-	
-	public static void myport() throws UnknownHostException{
-		System.out.println("My Port: "+client.getClientPort());
+
+	public static void myport() throws UnknownHostException {
+		System.out.println("My Port: " + client.getClientPort());
 	}
-	
-	public static void exit(){
-		if (client   != null)  	client.close();
+
+	public static void exit() {
+		if (client != null)
+			client.close();
 		System.exit(0);
 	}
-	
-}
 
+}
--- src/main/Server.java
+++ src/main/Server.java
@@ -1,33 +1,34 @@
 package main;
+
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
 public class Server extends Thread {
-	
-   private static ServerSocket serverSocket;
-   private static ArrayList<Socket> clients;
-   private boolean listenForConnections;
-   private boolean listenToClients;
-   private static Thread clientListen;
-   private DataInputStream  in   = null;
-   private DataOutputStream out = null;
-   private static Scanner inScan;
-   
-   public Server(int port) throws IOException {
-      serverSocket = new ServerSocket(port);
-      clients = new ArrayList<Socket>();
-      this.listenForConnections = true;
-      this.listenToClients = false;
-      
-   }
 
-   public void run() {
-      while(true) {
-    	 if (listenForConnections){
+	private static ServerSocket serverSocket;
+	private static ArrayList<Socket> clients;
+	private boolean listenForConnections;
+	private boolean listenToClients;
+	private static Thread clientListen;
+	private DataInputStream in = null;
+	private DataOutputStream out = null;
+	private static Scanner inScan;
+
+	public Server(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		clients = new ArrayList<Socket>();
+		this.listenForConnections = true;
+		this.listenToClients = false;
+
+	}
+
+	public void run() {
+		while (true) {
+			if (listenForConnections) {
 				try {
-					
+
 					System.out.println("Waiting for client on host " + this.getMyIP() + " on port "
 							+ serverSocket.getLocalPort() + "...");
 
@@ -41,131 +42,159 @@ public class Server extends Thread {
 					out = new DataOutputStream(clientSocket.getOutputStream());
 					out.writeUTF("Thank you for connecting to " + clientSocket.getLocalSocketAddress());
 
-					
 				} catch (SocketTimeoutException s) {
 					System.out.println("Socket timed out!");
-					//break;
+					// break;
 				} catch (SocketException se) {
 					System.out.println("Server Socket Closed");
-					//se.printStackTrace();
-					//break;
+					// se.printStackTrace();
+					// break;
 				} catch (IOException e) {
-					//e.printStackTrace();
-					//break;
-				} 
-    	 }
-    	 
-    	 clientListen = new Thread() {
-  		    public void run() {
-  		      while(true) {
-  		    	for(int i = 0; i < clients.size(); i++){
-  					try {
-  						// Checks if current client sent a command
-  						if (clients.get(i).getInputStream().available() != 0){
-  							DataInputStream in = new DataInputStream(clients.get(i).getInputStream());
-  							String input = in.readUTF();
-  							String[] args = input.split(" ");
-  							if (args[0].equals("list")){
-  						        sendClientsListToClient(i);
-  							}
-  							if (args[0].equals("send")){
-  								int toID = Integer.parseInt(args[1]);
-  								String msg = "";
-  								
-  								if (args.length > 2) {
-  									for(int j = 2; j < args.length; j++){
-  										if (j == args.length-1){
-  	  										msg += args[j];
-  										} else {
-  	  										msg += args[j] + " ";
-  										}
-  									}
-  								}
-  								
-  						        sendMsgToClient(i, toID, msg);
-  							}
-  						} 
-  					} catch (SocketException se) {
-  						System.out.println("Socket Closed");
-  						//se.printStackTrace();
-  					} catch (EOFException e) {
-  						//e.printStackTrace();
-  					} catch (IOException e) {
-  						//e.printStackTrace();
-  					}
-  					
-  					
-  	    		 }
-  		      }
-  		    }
-    	  };
-    	  clientListen.start();
-    	 
-    	 if(listenToClients){
-    	 }
-      }
-   }
-   
-   
-   
-   public InetAddress getMyIP() throws UnknownHostException{
+					// e.printStackTrace();
+					// break;
+				}
+			}
+
+			clientListen = new Thread() {
+				public void run() {
+					while (true) {
+						for (int i = 0; i < clients.size(); i++) {
+							try {
+								// Checks if current client sent a command
+								if (clients.get(i).getInputStream().available() != 0) {
+									DataInputStream in = new DataInputStream(clients.get(i).getInputStream());
+									String input = in.readUTF();
+									String[] args = input.split(" ");
+									if (args[0].equals("list")) {
+										sendClientsListToClient(i);
+									}
+									if (args[0].equals("send")) {
+										int toID = Integer.parseInt(args[1]);
+										String msg = "";
+
+										if (args.length > 2) {
+											for (int j = 2; j < args.length; j++) {
+												if (j == args.length - 1) {
+													msg += args[j];
+												} else {
+													msg += args[j] + " ";
+												}
+											}
+										}
+
+										sendMsgToClient(i, toID, msg);
+									}
+									if (args[0].equals("terminate")) {
+										int termID = Integer.parseInt(args[1]);
+										terminateClient(termID);
+										
+									}
+								}
+							} catch (SocketException se) {
+								System.out.println("Socket Closed");
+								// se.printStackTrace();
+							} catch (EOFException e) {
+								// e.printStackTrace();
+							} catch (IOException e) {
+								// e.printStackTrace();
+							}
+
+						}
+					}
+				}
+			};
+			clientListen.start();
+
+			if (listenToClients) {
+			}
+		}
+	}
+
+	public InetAddress getMyIP() throws UnknownHostException {
 		InetAddress address = null;
 		try {
 			address = InetAddress.getLocalHost();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 		return address;
 	}
-   
-   public static void getClients(){
-	   System.out.println(clients.size()+" clients connected");
-	   System.out.println("id: IP address			Port No.");
-	   for(int i = 0; i < clients.size(); i++) {   
-		    System.out.println(" "+i+"  "+clients.get(i).getInetAddress().getHostAddress()+"			"+clients.get(i).getPort());
-		}  
-   }
-   
-   private void sendClientsListToClient(int clientID){
-	   	System.out.println("Sending client id:"+clientID+" the list of clients...");
+
+	public static void getClients() {
+		System.out.println(clients.size() + " clients connected");
+		System.out.println("id: IP address			Port No.");
+		for (int i = 0; i < clients.size(); i++) {
+			System.out.println(" " + i + "  " + clients.get(i).getInetAddress().getHostAddress() + "			"
+					+ clients.get(i).getPort());
+		}
+	}
+
+	private void sendClientsListToClient(int clientID) {
+		System.out.println("Sending client id:" + clientID + " the list of clients...");
 		try {
 			out = new DataOutputStream(clients.get(clientID).getOutputStream());
-			out.writeUTF(clients.size()+" client(s) connected");
+			out.writeUTF(clients.size() + " client(s) connected");
 			out.writeUTF("id: IP address			Port No.");
-			for(int i = 0; i < clients.size(); i++) {   
-				out.writeUTF(" "+i+"  "+clients.get(i).getInetAddress().getHostAddress()+"			"+clients.get(i).getPort());
-			}  
+			for (int i = 0; i < clients.size(); i++) {
+				out.writeUTF(" " + i + "  " + clients.get(i).getInetAddress().getHostAddress() + "			"
+						+ clients.get(i).getPort());
+			}
 			out.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-   }
-   
-   private void sendMsgToClient(int fromID, int toID, String msg){
-	   	System.out.println("Sending to <"+toID+"> a message from <"+fromID+">");
+
+	}
+
+	private void sendMsgToClient(int fromID, int toID, String msg) {
+		System.out.println("Sending to <" + toID + "> a message from <" + fromID + ">");
 		try {
 			out = new DataOutputStream(clients.get(toID).getOutputStream());
-			out.writeUTF("Message received from "+clients.get(fromID).getInetAddress().getHostAddress());
-			out.writeUTF("Sender's Port: "+clients.get(fromID).getPort());
-			out.writeUTF("Message: '"+msg+"'");
+			out.writeUTF("Message received from " + clients.get(fromID).getInetAddress().getHostAddress());
+			out.writeUTF("Sender's Port: " + clients.get(fromID).getPort());
+			out.writeUTF("Message: '" + msg + "'");
 			out.flush();
 		} catch (IOException e) {
 			System.out.println("IO Exception has occured when sending a message!");
 		}
 		try {
 			out = new DataOutputStream(clients.get(fromID).getOutputStream());
-			out.writeUTF("Message sent to "+toID);
+			out.writeUTF("Message sent to " + toID);
 			out.flush();
 		} catch (IOException e) {
 			System.out.println("IO Exception has occured when sending a message!");
 		}
+
+	}
+
+	private void terminateClient(int termID) { 
+		System.out.println("Terminating Client " + termID);
+	
+		try {
+			Socket termSocket = new Socket();
+			out = new DataOutputStream(clients.get(termID).getOutputStream());
+			for(int i = 0; i < clients.size(); i++) {
+				if(termID == i) {
+					termSocket = clients.get(i);
+					clients.remove(termSocket);
+					termSocket.close();
+				}
+			}
+			out.writeUTF("You've been removed from the room");
+			out.flush();
+			termSocket.close();
+		} catch(SocketException e) {
+			System.out.println("Could not close socket");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-  }
-   
-   public static void close() {
-	   try {
-			for(int i = 0; i < clients.size(); i++) {   
+	
+	}
+	public static void close() {
+		try {
+			for (int i = 0; i < clients.size(); i++) {
 				clients.get(i).close();
 			}
 			serverSocket.close();
@@ -173,39 +202,37 @@ public class Server extends Thread {
 		} catch (IOException e) {
 			System.out.println("IO Exception has occured when closing!");
 		}
-		
-   }
- 
-   
-   public static void command(String command){
+
+	}
+
+	public static void command(String command) {
 		String[] args = command.split(" ");
 		switch (args[0]) {
-		case "close":  
+		case "close":
 			close();
 			break;
-		case "clients": 
+		case "clients":
 			getClients();
 			break;
 		}
-   }
-   
-   
-   public static void main(String [] args) {
-	      int port = Integer.parseInt(args[0]);
-	      try {
-	    	  
-	    	  Thread t = new Server(port);
-	    	  t.start();
+	}
 
-	      }catch(IOException e) {
-	         e.printStackTrace();
-	      }
-	      inScan = new Scanner(System.in);
-	      String input;
-	      do {
+	public static void main(String[] args) {
+		int port = Integer.parseInt(args[0]);
+		try {
+
+			Thread t = new Server(port);
+			t.start();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		inScan = new Scanner(System.in);
+		String input;
+		do {
 			input = inScan.nextLine();
 			command(input);
-	      } while(!input.equals("close"));
-   }
-   
+		} while (!input.equals("close"));
+	}
+
 }
